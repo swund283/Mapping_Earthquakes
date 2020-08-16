ce36ea60b3ce5b65adc88d736b225d3e65bfcf12
sam wunderilch <swund@MacBook-Pro.local>
2020-08-16 08:03:52 -0400

    earth 2

--- Earthquakes_past7days/static/js/logicStep3.js
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -32,17 +32,20 @@ L.control.layers(baseMaps).addTo(map);
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJson(data, {
-
-	// We turn each feature into a circleMarker on the map.
-	
-	pointToLayer: function(feature, latlng) {
-				  console.log(data);
-				  return L.circleMarker(latlng);
-			},
-		  // We set the style for each circleMarker using our styleInfo function.
-		style: styleInfo
-		}).addTo(map);
-	});
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+        console.log(data);
+        return L.circleMarker(latlng);
+      },
+    // We set the style for each circleMarker using our styleInfo function.
+  style: styleInfo,
+    // We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+    onEachFeature: function(feature, layer) {
+    layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+}
+}).addTo(map);
+});
 
 // This function returns the style data for each of the earthquakes we plot on
 // the map. We pass the magnitude of the earthquake into a function
@@ -55,7 +58,8 @@ function styleInfo(feature) {
 	  color: "#000000",
 	  radius: getRadius(feature.properties.mag),
 	  stroke: true,
-	  weight: 0.5
+	  weight: 0.5,
+	  fillColor: getColor(feature.properties.mag)
 	};
   }
 
@@ -67,3 +71,23 @@ function getRadius(magnitude) {
 	}
 	return magnitude * 4;
   }
+
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+	if (magnitude > 5) {
+	  return "#ea2c2c";
+	}
+	if (magnitude > 4) {
+	  return "#ea822c";
+	}
+	if (magnitude > 3) {
+	  return "#ee9c00";
+	}
+	if (magnitude > 2) {
+	  return "#eecc00";
+	}
+	if (magnitude > 1) {
+	  return "#d4ee00";
+	}
+	return "#98ee00";
+}
